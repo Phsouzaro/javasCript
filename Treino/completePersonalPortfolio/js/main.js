4b2ea6f9f2c96a07f3bea6ce3e58d0b336a45359
Pedro Henrique Souza <phsouzaro@gmail.com>
2022-01-12 22:59:59 -0300

    att portfolio

--- Treino/completePersonalPortfolio/js/main.js
+++ Treino/completePersonalPortfolio/js/main.js
@@ -3,3 +3,11 @@ window.addEventListener('scroll', function(){
  const header = document.querySelector('header')
  header.classList.toggle('sticky', window.scrollY > 0)
 })
+window.addEventListener('click', function(e){
+const target = e.target;
+if(target.classList.contains('learn-more-btn')||target.classList.contains('modal-close-btn')){
+ const learnMore = document.querySelector('.service-modal')
+ learnMore.classList.toggle('hidden')
+
+}
+})
