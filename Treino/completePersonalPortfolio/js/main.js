646e8db31ad51196f1de8b973318605ab441ae38
Pedro Henrique Souza <phsouzaro@gmail.com>
2022-01-25 23:17:46 -0300

    Atualizacao para o JS mostrar cada um dos elemntos com forEach, pois estava mostrando somente o primeiro elemento

--- Treino/completePersonalPortfolio/js/main.js
+++ Treino/completePersonalPortfolio/js/main.js
@@ -1,16 +1,3 @@
-/* Navigation  bar effects on scroll */
-/* window.addEventListener('scroll', function () {
- const header = document.querySelector('header')
- header.classList.toggle('sticky', window.scrollY > 0)
-})
-window.addEventListener('click', function (e) {
- const target = e.target;
- if (target.classList.contains('learn-more-btn') || target.classList.contains('modal-close-btn')) {
-  const learnMore = document.querySelector('.service-modal')
-  learnMore.classList.toggle('hidden')
- }
-}) */
-//Services Section
 const serviceModals = document.querySelectorAll('.service-modal')
 const learMoreBtns = document.querySelectorAll('.learn-more-btn')
 const modalCloseBtns = document.querySelectorAll('.modal-close-btn')
